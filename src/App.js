050de49f2b370cb31b5830f2d3d4098960623c3b
AstroGit <masaladosa6@gmail.com>
2022-01-15 12:12:39 +0300

    life circle, useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
-import React, { useMemo, useRef, useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
 import PostFilter from './components/PostFilter';
@@ -10,32 +11,29 @@ import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
 import MyModal from './components/UI/MyModal/MyModal';
 import MySelect from './components/UI/select/MySelect';
+import { usePosts } from './hooks/usePosts';
 import styles from './styles/App.css';
 
 function App() {
-const [posts, setPosts] = useState([
-  {id: 1, title: 'Javascript', body: 'Description'},
-  {id: 2, title: 'Javascript 2', body: 'Description'},
-  {id: 3, title: 'Javascript 3', body: 'Description'},
-])
+const [posts, setPosts] = useState([])
 const [filter, setFilter] = useState({sort: '', query: ''})
 const [modal, setModal] = useState(false);
+const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-const sortedPosts = useMemo(() => {
-  if(filter.sort) {
-    return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]));
-  }
-  return posts;
-}, [filter.sort, posts]);
+useEffect(() => {
+fetchPosts()
+}, [])
 
-const sortedAndSearchedPosts = useMemo(() => {
-return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-}, [filter.query, sortedPosts])
-
-  const createPost = (newPost) => {
+const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   }
+
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    setPosts(response.data)
+  }
+
 // получаем post из дочернего компонента
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
@@ -43,6 +41,7 @@ return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query
 
   return (
     <div className='App'>
+    <button onClick={fetchPosts}>GET POST</button>
     <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
       Создать пользователя
     </MyButton>
