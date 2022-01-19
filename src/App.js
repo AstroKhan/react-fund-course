d9ef84c5350029e4a86239c8523dd5bd1740fd5f
AstroGit <masaladosa6@gmail.com>
2022-01-19 11:46:48 +0300

    pagination and routing

--- src/App.js
+++ src/App.js
@@ -1,62 +1,26 @@
 
-import axios from 'axios';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import ClassCounter from './components/ClassCounter';
-import Counter from './components/Counter';
-import PostFilter from './components/PostFilter';
-import PostForm from './components/PostForm';
-import PostItem from './components/PostItem';
-import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
-import MyModal from './components/UI/MyModal/MyModal';
-import MySelect from './components/UI/select/MySelect';
-import { usePosts } from './hooks/usePosts';
-import styles from './styles/App.css';
+import React from 'react';
+import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom';
+import Navbar from './components/UI/Navbar/Navbar';
+import About from './pages/About';
+import Error from './pages/Error';
+import Posts from './pages/Posts';
+import './styles/App.css';
 
 function App() {
-const [posts, setPosts] = useState([])
-const [filter, setFilter] = useState({sort: '', query: ''})
-const [modal, setModal] = useState(false);
-const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-useEffect(() => {
-fetchPosts()
-}, [])
-
-const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  }
-
-  async function fetchPosts() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    setPosts(response.data)
-  }
-
-// получаем post из дочернего компонента
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
-
   return (
-    <div className='App'>
-    <button onClick={fetchPosts}>GET POST</button>
-    <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-      Создать пользователя
-    </MyButton>
-    <MyModal visible={modal} setVisible={setModal} >
-    <PostForm create={createPost} />
-    </MyModal>
-    
-    <hr style={{margin: '15px 0'}}/>
-    <PostFilter 
-    filter={filter} 
-    setFilter={setFilter} 
-    />
-    <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS' />
-    </div>
-  );
+    <BrowserRouter>
+    <Navbar/>
+
+    <Routes>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/posts" element={<Posts/>}/> 
+      <Route path="/404" element={<Error />} />
+      <Route path="*" element={<Navigate replace to="/404" />} />
+    </Routes>
+
+    </BrowserRouter>
+  )
 }
 
 export default App;
